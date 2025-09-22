85ef083848d031bd664916795488b7b46cc37607
Brain On Oxygen <brainonoxygen@gmail.com>
2025-09-22 10:16:41 +0800

    Add lib_boo
    Having issues dealing with submodules, so I'm adding the directory raw.

---
+++ src/lib_boo/components/scene_loader/SceneLoader.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var mainScn:PackedScene = null
+@onready var splash_screen: SplashScreen = $Splash_Screen
+
+
+func _ready():
+	if GM.config.ShouldSkipIntros and OS.is_debug_build():
+		LoadGame()
+
+func LoadGame():
+	get_tree().change_scene_to_packed.call_deferred(mainScn)
---
+++ src/lib_boo/global/game_manager/GameManager.gd
@@ -0,0 +1,25 @@
+class_name GameManager extends Node
+
+var main:MAIN
+var world:World
+
+@onready var textSpawner:SpawnerAct = $TextPopupSpawner
+@onready var timeGM:TimeManager = $TimeManager
+@onready var globalSpawner:GlobalSpawner = $GlobalSpawner
+@onready var globalAudio:GlobalAudioPlayer = $GlobalAudioPlayer
+@onready var saveLoad: BooSaveLoad = $BooSaveLoad
+
+@onready var events:GlobalEvents = $GlobalEvents
+@onready var config: GameConfig = $GameConfig
+
+func _ready() -> void:
+	pass
+
+func ShowTextPopup(text:String, parent:Node2D = world, start_pos:Vector2 = Vector2.ZERO, color:Color = Color.WHITE) -> TextPopup:
+	var popup = textSpawner.Execute({"parent":parent})[0] as TextPopup
+	popup.Play(text, start_pos, color)
+	return popup
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
